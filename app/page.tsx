45047f6adc1907ce6e7d4876ea030ea656146d59
Nattapong Sawatraksa <nattapong.sawa@gmail.com>
2025-06-09 00:40:42 +0700

    feat: authen flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+'use client';
+
 import Header from "@/components/Header";
 import {Box, Container} from "@mui/material";
 import JobBoard from "@/components/JobBoard";
+import {onAuthStateChanged} from "firebase/auth";
+import {auth} from "@/app/firebase";
+import { useRouter } from 'next/navigation';
+import {useEffect} from "react";
 
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (!currentUser) {
+                router.push('/login');
+            }
+        });
+        return () => unsubscribe(); // Cleanup subscription on unmount
+    }, [router]);
+
     return (
         <Container maxWidth="lg">
             <Box
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,35 @@
-import {Typography} from "@mui/material";
+'use client';
+
+import {Box, IconButton, Typography} from "@mui/material";
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import {auth} from "@/app/firebase";
 
 export default function Header() {
-    return <Typography variant="h4" component="h2">
+    const signOutUser = async () => {
+        try {
+            await auth.signOut();
+        } catch (err: any) {
+            console.error("Error signing out:", err);
+        }
+    }
+    return (<Box sx={{
+        display: 'flex',
+        direction: 'row',
+        alignItems: 'center',
+        px: 2,
+        py: 1,
+        width: '100%',
+    }}>
+        <Typography variant="h4" component="h2" sx={{
+            flexGrow: 1,
+            textAlign: 'center'
+        }}>
             Jib!❤️ This page is for you 🫵
         </Typography>
-
+        <IconButton aria-label="exit" onClick={signOutUser} color="error" sx={{
+            alignSelf: 'flex-start',
+        }}>
+            <ExitToAppIcon />
+        </IconButton>
+    </Box>);
 }

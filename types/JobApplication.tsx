abb1c2803e6d06c18f4c698c0fabbee6c73f0766
Nattapong Sawatraksa <nattapong.sawa@gmail.com>
2025-06-08 19:14:23 +0700

    feat: add job type and enum

--- types/JobApplication.tsx
+++
@@ -1,8 +0,0 @@
-export enum ApplicationStatus {
-    PENDING = 1,                  // รอยื่น
-    SUBMITTED = 2,               // ส่งใบสมัครแล้ว
-    SCHEDULED_INTERVIEW = 3,      // นัดสัมภาษณ์
-    INTERVIEWED = 4,             // สัมภาษณ์แล้ว
-    PASSED = 5,                  // ผ่าน
-    FAILED = 6                   // ไม่ผ่าน
-}
---
+++ types/job.tsx
@@ -0,0 +1,7 @@
+export type Job = {
+    id: string,
+    title: string,
+    description: string,
+    company: string,
+    url: string,
+}
